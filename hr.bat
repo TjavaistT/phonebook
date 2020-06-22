0c6bc2de903a91b46af0e609d79bae5db7ed2889
Vaist <tjavaistt@gmail.com>
2020-06-22 12:06:37 +0300

    - start from console for Windows!

---
+++ hr.bat
@@ -0,0 +1,2 @@
+call mvn -DskipTests=true clean package
+call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgresql://postgres:password@localhost:5432/phonebook" -DTOPJAVA_ROOT="." -jar target/dependency/webapp-runner.jar target/*.war
