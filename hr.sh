d58ac2c092bdd3858bfe54dab68f58ac08756edd
Vaist <tjavaistt@gmail.com>
2020-06-22 12:43:28 +0300

    - add Procfile(for heroku)

--- hr.sh
+++ hr.sh
@@ -1,2 +1,3 @@
 mvn -DskipTests=true clean package
-java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgresql://postgres:password@localhost:5432/phonebook" -DTOPJAVA_ROOT="." -jar target/dependency/webapp-runner.jar target/*.war
+#java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgresql://postgres:password@localhost:5432/phonebook" -DTOPJAVA_ROOT="." -jar target/dependency/webapp-runner.jar target/*.war
+java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://skusmswiplnkdk:e85b5f937a68f0b8482513201c02bffa3663b9b7d93fd1d95ad91aa90c99224d@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d47r2qtvn52i80" -DTOPJAVA_ROOT="." -jar target/dependency/webapp-runner.jar target/*.war
