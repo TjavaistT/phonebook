19eb3049e6de839231f06ea7be37f94cc87c2a92
Vaist <tjavaistt@gmail.com>
2020-06-28 11:24:36 +0300

    - change db.xml, hr.sh for run with Tomcat

--- hr.sh
+++ hr.sh
@@ -1,6 +1,2 @@
-#mvn -DskipTests=true clean package
 mvn -DskipTests=true clean dependency:list install
-#java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgresql://postgres:password@localhost:5432/phonebook" -DTOPJAVA_ROOT="." -jar target/dependency/webapp-runner.jar target/*.war
-#java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://skusmswiplnkdk:e85b5f937a68f0b8482513201c02bffa3663b9b7d93fd1d95ad91aa90c99224d@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d47r2qtvn52i80" -DTOPJAVA_ROOT="." -jar target/dependency/webapp-runner.jar target/*.war
-#java  -DDATABASE_URL="postgres://skusmswiplnkdk:e85b5f937a68f0b8482513201c02bffa3663b9b7d93fd1d95ad91aa90c99224d@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d47r2qtvn52i80" -jar target/dependency/webapp-runner.jar target/*.war
-java  -DPORT=8080 -DDATABASE_URL="postgres://skusmswiplnkdk:e85b5f937a68f0b8482513201c02bffa3663b9b7d93fd1d95ad91aa90c99224d@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d47r2qtvn52i80" -jar target/dependency/webapp-runner.jar target/*.war
+java  -DPORT=8080  -jar target/dependency/webapp-runner.jar target/*.war
